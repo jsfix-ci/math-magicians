76585278ca77c45f36b092724f71c18e6e15663b
franklin <franklinbenjamin032@gmail.com>
2022-03-19 18:54:54 -0400

    refractor calculate file to use hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.calculator = this.calculator.bind(this);
-    this.returnSection = this.returnSection.bind(this);
-  }
+const Calculator = () => {
+  const [digitObj, setDigitObj] = useState({
+    total: 0,
+    next: 0,
+    operation: null,
+  });
 
-  calculator = (obj, button) => {
-    const { updateState } = this.props;
+  const updateState = (obj) => {
+    setDigitObj(obj);
+  };
+
+  const calculator = (obj, button) => {
     const newObj = calculate(obj, button);
     updateState(newObj);
-  }
+  };
 
-  returnSection = () => {
-    const { digitObj } = this.props;
+  const returnSection = () => {
     const { total, next, operation } = digitObj;
 
     if (total === null && next === null) {
@@ -32,45 +33,46 @@ class Calculator extends React.Component {
       return total;
     }
     return total;
-  }
+  };
 
-  render() {
-    const { digitObj } = this.props;
-    return (
-      <section className="calculator">
-        <h1>Math Magician</h1>
-        <div className="return-section">{(this.returnSection())}</div>
-        <div className="digits-elts">
-          <button type="button" className="button-section" value="AC" onClick={() => { this.calculator(digitObj, 'AC'); }}>AC</button>
-          <button type="button" className="button-section" value="+/-" onClick={() => { this.calculator(digitObj, '+/-'); }}>+/-</button>
-          <button type="button" className="button-section" value="%" onClick={() => { this.calculator(digitObj, '%'); }}>%</button>
-          <button type="button" className="button-section operator" value="÷" onClick={() => { this.calculator(digitObj, '÷'); }}>÷</button>
-          <button type="button" className="button-section" value="7" onClick={() => { this.calculator(digitObj, '7'); }}>7</button>
-          <button type="button" className="button-section" value="8" onClick={() => { this.calculator(digitObj, '8'); }}>8</button>
-          <button type="button" className="button-section" value="9" onClick={() => { this.calculator(digitObj, '9'); }}>9</button>
-          <button type="button" className="button-section operator" value="x" onClick={() => { this.calculator(digitObj, 'x'); }}>x</button>
-          <button type="button" className="button-section" value="4" onClick={() => { this.calculator(digitObj, '4'); }}>4</button>
-          <button type="button" className="button-section" value="5" onClick={() => { this.calculator(digitObj, '5'); }}>5</button>
-          <button type="button" className="button-section" value="6" onClick={() => { this.calculator(digitObj, '6'); }}>6</button>
-          <button type="button" className="button-section operator" value="-" onClick={() => { this.calculator(digitObj, '-'); }}>-</button>
-          <button type="button" className="button-section" value="1" onClick={() => { this.calculator(digitObj, '1'); }}>1</button>
-          <button type="button" className="button-section" value="2" onClick={() => { this.calculator(digitObj, '2'); }}>2</button>
-          <button type="button" className="button-section" value="3" onClick={() => { this.calculator(digitObj, '3'); }}>3</button>
-          <button type="button" className="button-section operator" value="+" onClick={() => { this.calculator(digitObj, '+'); }}>+</button>
-        </div>
-        <span className="end">
-          <button type="button" className="button-section" value="0" onClick={() => { this.calculator(digitObj, '0'); }}>0</button>
-          <button type="button" className="button-section" value="." onClick={() => { this.calculator(digitObj, '.'); }}>.</button>
-          <button type="button" className="button-section operator" value="=" onClick={() => { this.calculator(digitObj, '='); }}>=</button>
-        </span>
-      </section>
-    );
-  }
-}
+  const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+'].map((btn) => (
+    (btn === 'x' || btn === '+' || btn === '-' || btn === '÷') ? (
+      <button
+        type="button"
+        className="button-section operator"
+        value={btn}
+        key={btn}
+        onClick={() => calculator(digitObj, btn)}
+      >
+        {btn}
+      </button>
+    ) : (
+      <button
+        type="button"
+        className="button-section"
+        value={btn}
+        key={btn}
+        onClick={() => calculator(digitObj, btn)}
+      >
+        {btn}
+      </button>
+    )
+  ));
 
-Calculator.propTypes = {
-  updateState: PropTypes.func.isRequired,
-  digitObj: PropTypes.oneOfType([PropTypes.object]).isRequired,
+  return (
+    <section className="calculator">
+      <h1>Calculator</h1>
+      <div className="return-section">{returnSection()}</div>
+      <div className="digits-elts">
+        {buttons}
+      </div>
+      <span className="end">
+        <button type="button" className="button-section" value="0" onClick={() => { calculator(digitObj, '0'); }}>0</button>
+        <button type="button" className="button-section" value="." onClick={() => { calculator(digitObj, '.'); }}>.</button>
+        <button type="button" className="button-section operator" value="=" onClick={() => { calculator(digitObj, '='); }}>=</button>
+      </span>
+    </section>
+  );
 };
 
 export default Calculator;
