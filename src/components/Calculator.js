d2ca1fc25daa10a3e74c82a276802ec81980bc43
franklin <franklinbenjamin032@gmail.com>
2022-03-19 17:17:46 -0400

    completely reestructure app

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,38 +1,40 @@
 import React from 'react';
-// eslint-disable-next-line react/prefer-stateless-function
+
 class Calculator extends React.Component {
-  createDigits = () => {
-    const digits = [];
-    for (let i = 1; i < 10; i += 1) {
-      digits.push(
-        <button key={i} type="button">{i}</button>,
-      );
-    }
-    return digits;
-  };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
   render() {
     return (
-      <div className="calculator">
-        <div className="display"> 0</div>
-
-        <div className="operators">
-          <button type="button">/</button>
-          <button type="button">*</button>
-          <button type="button">+</button>
-          <button type="button">-</button>
-
-          <button type="button">DEL</button>
+      <section className="calculator">
+        <h1 className="calc-header">Calculator</h1>
+        <div className="return-section">0</div>
+        <div className="digits-elts">
+          <button type="button" className="button-section">AC</button>
+          <button type="button" className="button-section">+/-</button>
+          <button type="button" className="button-section">%</button>
+          <button type="button" className="button-section operator">+</button>
+          <button type="button" className="button-section">7</button>
+          <button type="button" className="button-section">8</button>
+          <button type="button" className="button-section">9</button>
+          <button type="button" className="button-section operator">x</button>
+          <button type="button" className="button-section">4</button>
+          <button type="button" className="button-section">5</button>
+          <button type="button" className="button-section">6</button>
+          <button type="button" className="button-section operator">-</button>
+          <button type="button" className="button-section">1</button>
+          <button type="button" className="button-section">2</button>
+          <button type="button" className="button-section">3</button>
+          <button type="button" className="button-section operator">+</button>
         </div>
-
-        <div className="digits">
-          { this.createDigits() }
-          <button type="button">0</button>
-          <button type="button">.</button>
-          <button type="button">=</button>
-        </div>
-
-      </div>
+        <span className="end">
+          <button type="button" className="button-section">0</button>
+          <button type="button" className="button-section">.</button>
+          <button type="button" className="button-section operator">=</button>
+        </span>
+      </section>
     );
   }
 }
